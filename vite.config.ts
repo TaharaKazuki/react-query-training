b43d5943ab5f537bbbd7024c7821322ebd109068
Tahara Kazuki <>
2022-11-15 15:22:01 +0900

    update

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  base: './',
+  root: './src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [react()],
+})
